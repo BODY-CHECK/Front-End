3a9452154ac65472c592d50ef05d21c9abc00640
kshyun <sanghyun1201@naver.com>
2024-09-21 22:51:56 +0900

    #3 [FEAT] 바텀 네비게이션 구현

--- src/navigation/BottomTabNavigator.jsx
+++ src/navigation/BottomTabNavigator.jsx
@@ -1,15 +1,56 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Image} from 'react-native';
 import Home from '../screens/Home';
 import MyPage from '../screens/Mypage';
+import Routine from '../screens/Routine';
+import AIpt from '../screens/AIpt';
 
 const Tab = createBottomTabNavigator();
 
 function BottomTabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="MyPage" component={MyPage} />
+    <Tab.Navigator
+      initialRouteName="홈"
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused}) => {
+          let iconSource;
+
+          if (route.name === '홈') {
+            iconSource = focused
+              ? require('../assets/images/outline_home.png')
+              : require('../assets/images/icon_home.png');
+          } else if (route.name === 'AI 피티') {
+            iconSource = focused
+              ? require('../assets/images/outline_pt.png')
+              : require('../assets/images/icon_pt.png');
+          } else if (route.name === '운동 루틴') {
+            iconSource = focused
+              ? require('../assets/images/outline_routine.png')
+              : require('../assets/images/icon_routine.png');
+          } else if (route.name === '마이페이지') {
+            iconSource = focused
+              ? require('../assets/images/outline_mypage.png')
+              : require('../assets/images/icon_mypage.png');
+          }
+
+          return <Image source={iconSource} style={{width: 30, height: 30}} />;
+        },
+        tabBarActiveTintColor: '#3373EB',
+        tabBarInactiveTintColor: '#1C1B1F',
+        tabBarStyle: {
+          height: 70,
+          borderTopWidth: 1,
+          borderTopColor: '#E5E5E5',
+        },
+        tabBarLabelStyle: {
+          marginBottom: 8,
+        },
+      })}>
+      <Tab.Screen name="홈" component={Home} />
+      <Tab.Screen name="AI 피티" component={AIpt} />
+      <Tab.Screen name="운동 루틴" component={Routine} />
+      <Tab.Screen name="마이페이지" component={MyPage} />
     </Tab.Navigator>
   );
 }
--- src/navigation/LoginNavigator.jsx
+++ src/navigation/LoginNavigator.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from '../screens/Home';
 import Login from '../screens/Login';
+import BottomTabNavigator from './BottomTabNavigator';
 
 const Stack = createStackNavigator();
 
@@ -9,10 +10,12 @@ function LoginNavigator({isLoggedIn, setIsLoggedIn}) {
   return (
     <Stack.Navigator>
       {isLoggedIn ? (
-        // 로그인 된 상태에서는 홈 화면으로 이동
-        <Stack.Screen name="Home">
-          {props => <Home {...props} setIsLoggedIn={setIsLoggedIn} />}
-        </Stack.Screen>
+        // 로그인 된 상태에서는 바텀 네비게이터 표시
+        <Stack.Screen
+          name="Main"
+          component={BottomTabNavigator}
+          options={{headerShown: false}}
+        />
       ) : (
         // 로그인되지 않은 경우 로그인 화면을 표시
         <Stack.Screen name="Login" options={{headerShown: false}}>
