a775bb023a144cda8317847d495da40fbd238d39
kshyun <sanghyun1201@naver.com>
2024-10-03 20:39:22 +0900

    #11[FEAT]추천 루틴 추가

--- src/screens/Routine.jsx
+++ src/screens/Routine.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components/native';
 import TitlewithBtn from '../components/TitlewithBtn';
 import RoutineBox from '../components/home/RoutineBox';
 import DaySelector from '../components/routine/DaySelector';
+import ExerciseCard from '../components/routine/ExerciseCard';
 
 function Routine() {
   const [selectedDay, setSelectedDay] = useState('월'); // 선택된 요일을 관리하는 state
@@ -12,6 +13,7 @@ function Routine() {
       <TitlewithBtn title="MY 루틴" buttonText="설정하기>" />
       <DaySelector selectedDay={selectedDay} onDayChange={setSelectedDay} />
       <RoutineBox />
+      <ExerciseCard />
     </Container>
   );
 }
