8b49c014683f33416051bada974db8e4611e10ae
kshyun <sanghyun1201@naver.com>
2024-10-12 21:47:09 +0900

    #18[FEAT] 달력 커스텀

--- src/components/mypage/MyCalendar.jsx
+++ src/components/mypage/MyCalendar.jsx
@@ -1,6 +1,51 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import {Calendar} from 'react-native-calendars';
+import {Calendar, LocaleConfig} from 'react-native-calendars';
+
+// LocaleConfig에서 요일만 설정
+LocaleConfig.locales['ko'] = {
+  monthNames: [
+    'January',
+    'Feburary',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ],
+  monthNamesShort: [
+    'January',
+    'Feburary',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ],
+  dayNames: [
+    '일요일',
+    '월요일',
+    '화요일',
+    '수요일',
+    '목요일',
+    '금요일',
+    '토요일',
+  ],
+  dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+  today: '오늘',
+};
+
+LocaleConfig.defaultLocale = 'ko'; // 'ko'로 기본 로케일 설정
 
 const MyCalendar = () => {
   const [selectedDate, setSelectedDate] = useState('');
@@ -8,34 +53,22 @@ const MyCalendar = () => {
   return (
     <View style={styles.container}>
       <Calendar
-        // month 이름 및 화살표 버튼 스타일링
-        theme={{
-          calendarBackground: '#ffffff', // 캘린더 배경색
-          textSectionTitleColor: '#555', // 요일 이름 색상
-          textDayHeaderFontWeight: 'bold', // 요일 텍스트 두께
-          textDayHeaderFontSize: 14, // 요일 텍스트 크기
-          textMonthFontWeight: 'bold', // 월 이름 두께
-          textMonthFontSize: 18, // 월 텍스트 크기
-          arrowColor: 'gray', // 화살표 색상
-        }}
         // 날짜 선택 처리 및 마킹
         onDayPress={day => setSelectedDate(day.dateString)}
-        markedDates={{
-          [selectedDate]: {
-            selected: true,
-            selectedColor: '#1e90ff', // 선택된 날짜 배경색
-            selectedTextColor: '#ffffff', // 선택된 날짜 텍스트 색상
-          },
-          // 예시로 며칠 마킹
-          '2024-09-04': {marked: true, dotColor: '#1e90ff'},
-          '2024-09-05': {marked: true, dotColor: '#1e90ff'},
-          '2024-09-06': {marked: true, dotColor: '#1e90ff'},
-          '2024-09-02': {marked: true, dotColor: '#1e90ff'},
-          '2024-09-03': {marked: true, dotColor: '#1e90ff'},
-        }}
+        markedDates={
+          selectedDate
+            ? {
+                [selectedDate]: {
+                  selected: true,
+                  selectedColor: '#3373eb', // 선택된 날짜 배경색
+                  selectedTextColor: '#ffffff', // 선택된 날짜 텍스트 색상
+                },
+              }
+            : {}
+        }
         // 기본 스타일링
         style={{
-          width: 400,
+          width: 380,
         }}
       />
     </View>
