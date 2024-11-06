0d4939c8a68cfd02e1ed2a04c73a38d910671a38
kshyun <sanghyun1201@naver.com>
2024-11-06 17:38:59 +0900

    [feat]리프레쉬토큰을 통해 accesstoken 재발급 로직 추가

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,25 +1,71 @@
 // AuthContext.js
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, {createContext, useState, useContext, useEffect} from 'react';
+import instance from './axiosInstance';
+import {Alert, AppState} from 'react-native';
+
+const baseURL = 'https://dev.bodycheck.store'; // 서버 주소
 
 const AuthContext = createContext();
 
 export function AuthProvider({children}) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // 앱 로드 시 토큰이 있는지 확인하여 로그인 상태 복원
+  // 로그아웃 함수
+  const logout = async () => {
+    await AsyncStorage.removeItem('accessToken');
+    await AsyncStorage.removeItem('refreshToken');
+    setIsLoggedIn(false);
+    Alert.alert('다시 로그인해주세요');
+  };
+
+  // 액세스 토큰 새로고침 함수
+  const refreshAccessToken = async () => {
+    const refreshToken = await AsyncStorage.getItem('refreshToken');
+    if (refreshToken) {
+      try {
+        const refreshResponse = await instance.post(
+          `${baseURL}/members/refresh-token`,
+          {refreshToken},
+        );
+        if (refreshResponse.data.isSuccess) {
+          const {accessToken} = refreshResponse.data.result;
+          await AsyncStorage.setItem('accessToken', accessToken);
+          setIsLoggedIn(true); // 로그인 상태 설정
+        } else {
+          await logout();
+        }
+      } catch (error) {
+        console.error('토큰 갱신 오류:', error);
+        await logout();
+      }
+    }
+  };
+
+  // 포그라운드 전환 시 토큰 확인 및 갱신
   useEffect(() => {
-    const checkToken = async () => {
-      const accessToken = await AsyncStorage.getItem('accessToken');
-      if (accessToken) {
-        setIsLoggedIn(true); // 토큰이 있으면 로그인 상태 설정
+    const handleAppStateChange = async nextAppState => {
+      if (nextAppState === 'active') {
+        const accessToken = await AsyncStorage.getItem('accessToken');
+        if (!accessToken) {
+          await refreshAccessToken(); // 액세스 토큰 갱신
+        } else {
+          setIsLoggedIn(true);
+        }
       }
     };
-    checkToken();
+
+    // 앱 상태 변화 감지
+    const subscription = AppState.addEventListener(
+      'change',
+      handleAppStateChange,
+    );
+
+    return () => subscription.remove();
   }, []);
 
   return (
-    <AuthContext.Provider value={{isLoggedIn, setIsLoggedIn}}>
+    <AuthContext.Provider value={{isLoggedIn, setIsLoggedIn, logout}}>
       {children}
     </AuthContext.Provider>
   );
