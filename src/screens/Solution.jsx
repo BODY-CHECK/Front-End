aa46550d29803a9a1bba416a3a62bd44a35c713c
bbky323 <bbky323@kau.kr>
2024-11-22 17:02:09 +0900

    [STYLE]#42 그래프 최대값 스타일 수정

--- src/screens/Solution.jsx
+++ src/screens/Solution.jsx
@@ -42,16 +42,6 @@ export default function Solution() {
     fetchSolution();
   }, [id]);
 
-  useFocusEffect(() => {
-    navigation.getParent()?.setOptions({
-      tabBarStyle: {display: 'none'},
-    });
-
-    return () =>
-      navigation.getParent()?.setOptions({
-        tabBarStyle: undefined,
-      });
-  });
 
   const chartConfig = {
     backgroundGradientFrom: '#fff',
@@ -97,9 +87,9 @@ export default function Solution() {
               source={{uri: `${solutionData.solutionVideoUrl}.mp4`}} // 비디오 파일의 URL 또는 로컬 파일 경로
               style={styles.video}
               controls={true} // 기본 컨트롤러 표시 (재생, 일시정지, 탐색바 등)
-              resizeMode="cover" // 비디오 크기 조절 방식 ('cover', 'contain', 'stretch' 등)
-              paused={false} // true일 경우 비디오가 일시정지됨
-              repeat={true} // 비디오 반복 재생
+              resizeMode="contain" // 비디오 크기 조절 방식 ('cover', 'contain', 'stretch' 등)
+              paused={true} // true일 경우 비디오가 일시정지됨
+              repeat={false} // 비디오 반복 재생
             />
           ) : (
             <ContentText>비디오가 없습니다.</ContentText>
@@ -109,7 +99,7 @@ export default function Solution() {
           <BarChart
             style={graphStyle}
             data={{
-              labels: ['팔 각도', '자세 장렬', '무릎 각도'],
+              labels: ['팔 각도', '자세 장렬', '무릎 각도', ],
               datasets: [
                 {
                   data: solutionData?.criteriaDetailList.map(
@@ -125,6 +115,7 @@ export default function Solution() {
             verticalLabelRotation={0}
             withInnerLines={true}
             withHorizontalLabels={true}
+            fromNumber={100}
           />
         </GraphContainer>
         <TextContainer>
@@ -146,7 +137,7 @@ const styles = StyleSheet.create({
   },
   video: {
     width: '100%',
-    height: '100%',
+    height: 200,
     borderWidth: 1,
     borderColor: 'black',
   },
